833af62ae7073e550b19a7b7b1b6cc738059cccb
kkleia <retread.golf-07@icloud.com>
2025-05-12 15:11:44 +0800

    更新依赖项，添加 base64 和 rand 库；修改主程序以生成随机字符串并在 HTML 中显示；新增 location.html 文件以展示 Fly.io 全球服务器位置地图

--- bench.sh
+++ bench.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# 定义非美国地区
-REGIONS="ams bog otp eze fra gdl hkg jnb lhr mad yul bom cdg gig scl gru sin arn syd nrt yyz waw"
-
-# 循环为每个地区设置 1 个 Machine
-for region in $REGIONS; do
-  echo "Scaling Machine in $region..."
-  fly scale count 1 --region $region
-  sleep 2
+for r in hkg bom sin syd nrt iad sjc yyz scl fra lhr jnb; do
+  fly scale count 1 -r $r
 done
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-hello-world
-//! ```
-
 use axum::{response::Html, routing::get, Router};
+use rand::RngCore;
+use base64::{engine::general_purpose, Engine as _};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
     let app = Router::new().route("/", get(handler));
 
-    // run it
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
@@ -19,6 +13,13 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn handler() -> Html<String> {
+    let random_string = generate_random_string();
+    Html(format!("<h1>Random: {}</h1>", random_string))
+}
+
+fn generate_random_string() -> String {
+    let mut bytes = [0u8; 16];
+    rand::thread_rng().fill_bytes(&mut bytes);
+    general_purpose::STANDARD.encode(bytes)
 }
